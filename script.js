e04ed64ca0d287c8d30bdcb82026421abfc959b4
sideline. <nkhaval@mail.ru>
2025-08-29 00:01:56 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
-function typeWriter(el, text, speed=50) {
+function typeWriter(el, text, speed = 50, callback) {
   let i = 0;
   function typing() {
     if (i < text.length) {
       el.innerHTML += text.charAt(i);
       i++;
       setTimeout(typing, speed);
+    } else if (callback) {
+      callback();
     }
   }
   typing();
 }
-
 document.addEventListener("DOMContentLoaded", () => {
-  typeWriter(document.querySelector(".right-hero h1"), "Programmer", 120);
-  typeWriter(document.querySelector(".left-hero h1"), "Graphic Designer", 120);
+  typeWriter(document.querySelector(".iGot"), "i got", 70);
+
+  setTimeout(() => {
+    typeWriter(document.querySelector(".blackk"), "Black", 80);
+  }, 500);
+
+  setTimeout(() => {
+    typeWriter(document.querySelector(".whitee"), "White", 80);
+  }, 1100);
+
+  setTimeout(() => {
+    typeWriter(document.querySelector(".whatYouWant"), "what you want?", 35);
+  }, 1650);
 });
+
