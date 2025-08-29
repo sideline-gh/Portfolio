932c7233b96dc8620f18dfe0eccbf6fa97e7fd48
sideline. <nkhaval@mail.ru>
2025-08-29 21:48:10 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const images = document.querySelectorAll(".case-img");
+const arrowLeft = document.getElementById("arrowLeft");
+const arrowRight = document.getElementById("arrowRight");
+const slider = document.querySelector(".slider");
 function typeWriter(el, text, speed = 50, callback) {
   let i = 0;
   function typing() {
@@ -26,4 +30,32 @@ document.addEventListener("DOMContentLoaded", () => {
     typeWriter(document.querySelector(".whatYouWant"), "what you want?", 35);
   }, 1650);
 });
+let current = 0;
 
+function updateSlider() {
+  slider.style.transform = `translateX(-${current * 100}%)`;
+  updateText()
+}
+
+arrowLeft.addEventListener("click", () => {
+  current = (current - 1 + images.length) % images.length;
+  updateSlider();
+});
+
+arrowRight.addEventListener("click", () => {
+  current = (current + 1) % images.length;
+  updateSlider();
+});
+
+const imageTexts = document.querySelectorAll('.imgText')
+
+function updateText() {
+  imageTexts.forEach(el => el.classList.add("textDisabled"));
+  
+  if (imageTexts[current]){
+    imageTexts[current].classList.add('textEnabled')
+    imageTexts[current].classList.remove('textDisabled')
+  }
+}
+
+updateText();
